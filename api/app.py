123084278d6652ad8a06ac94aeb89a65e916b79a
markyangkp <markyangkp@outlook.com>
2024-10-14 20:21:40 +0800

    app引用

--- api/app.py
+++ api/app.py
@@ -1,5 +1,33 @@
-from fastapi import FastAPI
 from dotenv import load_dotenv
 from configs.llm_config import llm_Settings
+from fastapi import FastAPI, APIRouter
+from starlette.middleware.cors import CORSMiddleware
+import uvicorn
+from controllers import chat2note_router
 
-print(llm_Settings.OPENAI_API_KEY)
+origins = [
+    "http://localhost:5173",  # 你的 Vite 项目
+    "http://localhost:3000",  # 其他本地开发环境端口
+    # 可以根据需要添加其他来源
+]
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,            # 允许的域名
+    allow_credentials=True,           # 允许携带凭证
+    allow_methods=["*"],              # 允许的 HTTP 方法，如 GET, POST 等
+    allow_headers=["*"],              # 允许的请求头
+)
+
+
+router = APIRouter()
+router.include_router(chat2note_router, prefix="/chat2note", tags=["mark", "chat2note"])
+
+app.include_router(router, prefix="/v1/api", tags=["mark"])
+
+# print(llm_Settings.OPENAI_API_KEY)
+if __name__ == "__main__":
+  
+  config = uvicorn.Config("app:app", host="0.0.0.0", port=9988, reload=True)
+  server = uvicorn.Server(config)
+  server.run()
