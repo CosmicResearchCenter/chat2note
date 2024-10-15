bac8c3b760076710c03ec1647b9054ad14283bc8
markyangkp <markyangkp@outlook.com>
2024-10-15 23:03:20 +0800

    搭建初步界面，未链接后端

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,11 +1,47 @@
-import './assets/main.css'
+// import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// 引入markdown解析插件
+// 创建一个声明文件来避免TypeScript编译错误
+// declare module '@kangc/v-md-editor' {
+//     const content: any;
+//     export default content;
+// };
+
+// 引入markdown解析插件
+// 创建一个声明文件来避免TypeScript编译错误
+declare module '@kangc/v-md-editor' {
+    const content: any;
+    export default content;
+};
+
+// import VMdEditor from '@kangc/v-md-editor';
+import VMdPreview from '@kangc/v-md-editor/lib/preview';
+
+import '@kangc/v-md-editor/lib/style/base-editor.css';
+import githubTheme from '@kangc/v-md-editor/lib/theme/github.js';
+import '@kangc/v-md-editor/lib/theme/style/github.css';
+import hljs from 'highlight.js';
+VMdPreview.use(githubTheme, {
+    Hljs: hljs,
+});
+
+import VMdEditor from '@kangc/v-md-editor';
+VMdEditor.use(githubTheme, {
+    Hljs: hljs,
+});
+
+
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
 const app = createApp(App)
 
 app.use(router)
+app.use(VMdEditor);
+app.use(ElementPlus)
 
 app.mount('#app')
--- web/src/views/HomeView.vue
+++ web/src/views/HomeView.vue
@@ -1,9 +1,27 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="Main">
+    <div class="chatUrlBox">
+      <el-input v-model="url" style="width: 440px" placeholder="Please input" />
+      <el-button @click="">获取</el-button>
+    </div>
+    <v-md-editor v-model="text" height="100vh"></v-md-editor>
+  </div>
 </template>
+<script lang="ts" setup>
+import { ref } from 'vue'
+const text = ref("123")
+const url = ref("123")
+</script>
+<style scoped>
+.Main{
+  display: flex;
+  flex-direction: column;
+  justify-self: center;
+  align-items: center;
+}
+.chatUrlBox{
+  display: flex;
+  flex-direction: row;
+}
+</style>
