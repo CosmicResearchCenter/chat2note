47c47994867a1d8313fed245b5d144478bbd0fd1
markyangkp <markyangkp@outlook.com>
2024-10-16 17:22:05 +0800

    修改标结构

--- api/models/llm_info.py
+++ api/models/llm_info.py
@@ -1,15 +1,16 @@
 from sqlalchemy import Column, Float, Integer, String, Text,TIMESTAMP,Boolean,JSON
 from database.mysql_client import Base
+from sqlalchemy.sql import func
 
 class Api_Keys(Base):
     __tablename__ = "api_keys"
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     provider = Column(String(255))
     api_key = Column(String(255)) 
     config = Column(JSON)
-    created_at = Column(TIMESTAMP)
-    updated_at = Column(TIMESTAMP) 
+    created_at = Column(TIMESTAMP, default=func.now())
+    updated_at = Column(TIMESTAMP, default=func.now(), onupdate=func.now())
 
 class Note(Base):
     __tablename__ = "notes"
