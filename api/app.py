1280d7fa8733771bb80be6e7815acb0975d75add
markyangkp <markyangkp@outlook.com>
2024-10-17 00:23:25 +0800

    跨域放行

--- api/app.py
+++ api/app.py
@@ -6,9 +6,7 @@ import uvicorn
 from controllers import chat2note_router
 
 origins = [
-    "http://localhost:5173",  # 你的 Vite 项目
-    "http://localhost:3000",  # 其他本地开发环境端口
-    # 可以根据需要添加其他来源
+    "*"
 ]
 app = FastAPI()
 app.add_middleware(
