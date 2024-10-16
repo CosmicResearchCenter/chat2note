8c09a8a9cf00dc1b3404caf2130bb22bb1270715
markyangkp <markyangkp@outlook.com>
2024-10-16 10:11:32 +0800

    新增表

--- api/controllers/chat2note/models.py
+++ api/controllers/chat2note/models.py
@@ -2,9 +2,15 @@ from pydantic import BaseModel
 from typing import List,Dict,Any
 class ChatLogRequest(BaseModel):
     url:str
+    provider:str
     steaming:bool=False
     
 class ChatLogResponse(BaseModel):
+    code:str
+    message:str
+    data:List[Any]
+
+class ProvidersResponse(BaseModel):
     code:str
     message:str
     data:List[Any]
